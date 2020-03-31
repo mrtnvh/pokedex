5882dd65a3a98490cc257d54abc48a383435eba8
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2020-03-31 23:36:18 +0200

    feat(PokemonGrid): Don't use placeholders

--- components/PokemonGrid.vue
+++ components/PokemonGrid.vue
@@ -3,18 +3,9 @@
     <div
       v-for="({ image, name }, index) in items"
       :key="index"
-      class="text-center bg-white flex flex-col justify-center items-center p-4"
+      class="text-center bg-white flex flex-col justify-center items-center p-4 shadow-lg rounded-md"
     >
-      <div class="w-24 h-24">
-        <div class="relative pb-1/1 w-full">
-          <img
-            :src="image"
-            :alt="name"
-            loading="lazy"
-            class="absolute top-0 left-0 w-full h-full"
-          />
-        </div>
-      </div>
+      <img :src="image" :alt="name" loading="lazy" width="96" height="96" />
       <div class="mt-2 font-bold">{{ name }}</div>
     </div>
   </div>
